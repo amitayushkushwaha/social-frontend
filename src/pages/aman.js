1f9d4a4fd197951cb7b39b2086d52ac4aeb24c2e
hooknb <hook.nishant@gmail.com>
2023-02-12 12:44:22 +0530

    folder structure

---
+++ src/pages/aman.js
@@ -0,0 +1,16 @@
+import Trending from "@/components/Trending";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Social Media/Aman</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main>
+        <Trending />
+      </main>
+    </>
+  );
+}
---
+++ src/pages/amit.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Social Media/Amit</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main>Starter</main>
+    </>
+  );
+}
