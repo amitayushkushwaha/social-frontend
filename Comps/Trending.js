5de01d51def6d822b51d677f58be2b973e37944c
the-day-dreamer <103681398+the-day-dreamer@users.noreply.github.com>
2023-02-12 12:10:56 +0530

    created the trending component

---
+++ Comps/Trending.js
@@ -0,0 +1,24 @@
+import styles from '../src/styles/Trending.module.css'
+const Trending = () => {
+    return ( 
+        <div className="trending">
+            <div className={styles.buttonBox}>
+                <button className={styles.trendingBtn}>
+                    Trending
+                </button>
+            </div>
+            <hr />
+            <div className="topics">#topics</div>
+            <div className={styles.content}>
+                <div className={styles.contentBox}></div>
+                <div className={styles.contentBox}></div>
+                <div className={styles.contentBox}></div>
+                <div className={styles.contentBox}></div>
+            </div>
+            <hr />
+            <div className="posts">#posts</div>
+        </div>
+     );
+}
+ 
+export default Trending;
