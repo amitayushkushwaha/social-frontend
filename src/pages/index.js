299327d03a903c3fd506ac2933b87ea9613f70c7
hooknb <hook.nishant@gmail.com>
2023-02-12 12:31:59 +0530

    home pages chages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
-import Trending from 'Comps/Trending'
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
-    
-    <div className="home">
-      <Trending/>
-    </div>
-  )
+    <>
+      <Head>
+        <title>Social Media</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main className={styles.main}>Starter</main>
+    </>
+  );
 }
