2688811f46afda12ff382ad315943cdc34662748
the-day-dreamer <103681398+the-day-dreamer@users.noreply.github.com>
2023-02-14 17:16:44 +0530

    created the navbar component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,8 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Social Media</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <main className={styles.main}>Starter</main>
-    </>
+    <div className="home">
+      
+    </div>
   );
 }
