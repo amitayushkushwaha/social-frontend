c6a5b750bbcace7915a4fe9f8ddb43020b7f9f2a
hooknb <nishant17bhagat@outlook.com>
2023-03-05 15:42:06 +0530

    remove conflicts

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,53 +1,65 @@
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import {motion} from 'framer-motion'
+import "./../styles/Navbar.module.css";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { motion } from "framer-motion";
+
 const Topbar = () => {
-    
-    return ( 
-        
-            <motion.div className="topbarBox" id='topbarBoxId' initial = {{opacity:0}} animate = {{opacity:1}}>
-                <motion.div
-                initial = {{x:-100}}
-                animate = {{x:5}}
-                transition = {{duration:.5}}
-                >
-                <AccountCircleIcon fontSize='large'/>
-                </motion.div>
-                <motion.div className="appName" 
-                initial = {{x:'-100vw'}}
-                animate = {{x:0}}
-                transition = {{duration:1}}
-                >AppName</motion.div>
-                <motion.p className="para" 
-                initial = {{x:'-100vw'}}
-                animate = {{x:0}}
-                transition = {{duration:1}}
-                
-                >goodmorning!!</motion.p>
-                <motion.button className="topbarButton" initial = {{opacity : 0}} animate = {{opacity:1}} transition = {{duration:1}} >View Account</motion.button>
-                <div className="color1">
+  return (
+    <motion.div
+      className="topbarBox"
+      id="topbarBoxId"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+    >
+      <motion.div
+        initial={{ x: -100 }}
+        animate={{ x: 5 }}
+        transition={{ duration: 0.5 }}
+      >
+        <AccountCircleIcon fontSize="large" />
+      </motion.div>
+      <motion.div
+        className="appName"
+        initial={{ x: "-100vw" }}
+        animate={{ x: 0 }}
+        transition={{ duration: 1 }}
+      >
+        AppName
+      </motion.div>
+      <motion.p
+        className="para"
+        initial={{ x: "-100vw" }}
+        animate={{ x: 0 }}
+        transition={{ duration: 1 }}
+      >
+        Good Morning!!
+      </motion.p>
+      <motion.button
+        className="topbarButton"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
+        View Account
+      </motion.button>
+      <div className="color1"></div>
+      <div className="study">Study</div>
+      <hr />
+      <div className="studyBox">
+        <motion.div
+          className="box1"
+          initial={{ x: "-50vw" }}
+          animate={{ x: 0 }}
+          transition={{ duration: 1 }}
+        ></motion.div>
+        <motion.div
+          className="box2"
+          initial={{ x: "50vw" }}
+          animate={{ x: 0 }}
+          transition={{ duration: 1 }}
+        ></motion.div>
+      </div>
+    </motion.div>
+  );
+};
 
-                </div>
-                <div className="study">
-                    Study
-                </div>
-                <hr />
-                <div className="studyBox">
-                    <motion.div className="box1" 
-                    initial = {{x:'-50vw'}}
-                    animate = {{x:0}}
-                    transition = {{duration:1}}
-
-                    ></motion.div>
-                    <motion.div className="box2"
-                    initial = {{x:'50vw'}}
-                    animate = {{x:0}}
-                    transition = {{duration:1}}
-
-                    ></motion.div>
-                </div>
-            </motion.div>
-        
-     );
-}
- 
 export default Topbar;
--- src/pages/amogh.js
+++ src/pages/amogh.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Post from "@/components/Post";
 import SuggestionBox from "@/components/SuggestionBox";
+import Navbar from "@/components/Navbar";
 // import styles from "@/styles/globals.css"
 export default function Post1() {
   return (
@@ -10,18 +11,14 @@ export default function Post1() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main>
-        <SuggestionBox id={"i"}/>
-        <Post username={"Amogh"} hasImage={true} postId={'i'}/>
-        <button className="trendingBtn">   Trending  ../</button>
-        <Post username={"Amit"} hasImage={true} postId={'i'}/>
-        <Post username={"Aman"} hasImage={true} postId={'i'}/>
-        <Post username={"Nishant"} hasImage={true} postId={'i'}/>
-        
+        <Navbar />
+        <SuggestionBox id={"i"} />
+        <Post username={"Amogh"} hasImage={true} postId={"i"} />
+        <button className="trendingBtn"> Trending ../</button>
+        <Post username={"Amit"} hasImage={true} postId={"i"} />
+        <Post username={"Aman"} hasImage={true} postId={"i"} />
+        <Post username={"Nishant"} hasImage={true} postId={"i"} />
       </main>
     </>
   );
 }
-
-
-
-
