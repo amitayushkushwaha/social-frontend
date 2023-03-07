a577e6b9724d665dd5edca5b8f0797325fe29a22
the-day-dreamer <103681398+the-day-dreamer@users.noreply.github.com>
2023-03-07 15:02:22 +0530

    moded the account icon from topbar to navbar and added colors

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,5 +1,5 @@
 import "./../styles/Navbar.module.css";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+
 import { motion , AnimatePresence } from "framer-motion";
 import { useState } from "react";
 import Sidebar from "./Sidebar";
@@ -21,40 +21,10 @@ const Topbar = () => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
     >
-      <AnimatePresence>
-      {leftBar && (<motion.div className={styles.preSidebar}
-      initial = {{x:'-100vw'}}
-      animate = {{x:0}}
-      transition = {{duration : 1}}
-      exit={{x:'-100vw',transition:{duration:1}}}
-      ><Sidebar leftBar = {leftBar} setLeftBar = {setLeftBar}></Sidebar></motion.div>)}
-      </AnimatePresence>
-      <motion.span
-        onClick={handleSideBar}
-        whileHover = {{scale : 1.3}}
-      >
-        <AccountCircleIcon fontSize="large" />
-      </motion.span>
-      <motion.div
-        className="appName"
-        initial={{ x: "-100vw" }}
-        animate={{ x: 0 }}
-        transition={{ duration: 1 }}
-      >
-        AppName
-      </motion.div>
-      <motion.p
-        className="para"
-        initial={{ x: "-100vw" }}
-        animate={{ x: 0 }}
-        transition={{ duration: 1 }}
-      >
-        Good Morning!!
-      </motion.p>
       
-      <div className="color1"></div>
-      <div className="study">Study</div>
-      <hr />
+      
+      
+      
       <div className="studyBox">
         <motion.div
           className="box1"
